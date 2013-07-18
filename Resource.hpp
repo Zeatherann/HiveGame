daa21c245793f762f6c30a4e35def48d0b592ddc
Michael Jerrick <chaoszinc@mindspring.com>
2013-07-17 23:17:00 -0600

    Worked on Resource.

--- Resource.hpp
+++ Resource.hpp
@@ -8,7 +8,10 @@ public:
     // Functions
     ResourceType& Load(const std::string& Name)
     {
-        return Data[Name];
+        ResourceType& Ret=Data[Name];
+        if(Ret.loadFromFile(Name))return Ret;
+        cerr<<"Failed to load resource '"<<Ret<<"'!"<<endl;
+        exit(1);
     }
     // Operators
     ResourceType& operator[](const std::string& Name)
